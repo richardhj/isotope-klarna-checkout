6405e956336d86f6421ae58af10258cb621344d1
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2018-02-14 12:34:00 +0100

    Add en language files

---
+++ src/Resources/contao/languages/en/modules.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * This file is part of richardhj/isotope-klarna-checkout.
+ *
+ * Copyright (c) 2018-2018 Richard Henkenjohann
+ *
+ * @package   richardhj/isotope-klarna-checkout
+ * @author    Richard Henkenjohann <richardhenkenjohann@googlemail.com>
+ * @copyright 2018-2018 Richard Henkenjohann
+ * @license   https://github.com/richardhj/isotope-klarna-checkout/blob/master/LICENSE LGPL-3.0
+ */
+
+
+$GLOBALS['TL_LANG']['FMD']['iso_klarna_checkout'][0]              = 'Klarna Checkout';
+$GLOBALS['TL_LANG']['FMD']['iso_klarna_checkout'][1]              = 'Implements the Klarna Checkout iFrame to the shop.';
+$GLOBALS['TL_LANG']['FMD']['iso_klarna_checkout_confirmation'][0] = 'Klarna Checkout confirmation';
+$GLOBALS['TL_LANG']['FMD']['iso_klarna_checkout_confirmation'][1] = 'Implements the Klarna Checkout confirmation to the shop.';
