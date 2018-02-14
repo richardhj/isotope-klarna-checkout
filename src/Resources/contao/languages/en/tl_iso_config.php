c9413c5dd142f2df79fabc1d0712266d3fd5d4ca
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2018-02-14 12:29:48 +0100

    Add en language files

---
+++ src/Resources/contao/languages/en/tl_iso_config.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * This file is part of richardhj/isotope-klarna-checkout.
+ *
+ * Copyright (c) 2018-2018 Richard Henkenjohann
+ *
+ * @package   richardhj/isotope-klarna-checkout
+ * @author    Richard Henkenjohann <richardhenkenjohann@googlemail.com>
+ * @copyright 2018-2018 Richard Henkenjohann
+ * @license   https://github.com/richardhj/isotope-klarna-checkout/blob/master/LICENSE LGPL-3.0
+ */
+
+
+$GLOBALS['TL_LANG']['tl_iso_config']['klarna_legend'] = 'Klarna';
+
+$GLOBALS['TL_LANG']['tl_iso_config']['use_klarna'][0]          = 'Use Klarna';
+$GLOBALS['TL_LANG']['tl_iso_config']['use_klarna'][1]          = 'Enable Klarna Checkout v3 support for this shop configuration.';
+$GLOBALS['TL_LANG']['tl_iso_config']['klarna_api_username'][0] = 'Klarna API username';
+$GLOBALS['TL_LANG']['tl_iso_config']['klarna_api_username'][1] = 'Enter the API username that you can generate within the Klarna merchant portal.';
+$GLOBALS['TL_LANG']['tl_iso_config']['klarna_api_password'][0] = 'Klarna API password';
+$GLOBALS['TL_LANG']['tl_iso_config']['klarna_api_password'][1] = 'Enter the API password that you can generate within the Klarna merchant portal.';
---
+++ src/Resources/contao/languages/en/tl_module.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * This file is part of richardhj/isotope-klarna-checkout.
+ *
+ * Copyright (c) 2018-2018 Richard Henkenjohann
+ *
+ * @package   richardhj/isotope-klarna-checkout
+ * @author    Richard Henkenjohann <richardhenkenjohann@googlemail.com>
+ * @copyright 2018-2018 Richard Henkenjohann
+ * @license   https://github.com/richardhj/isotope-klarna-checkout/blob/master/LICENSE LGPL-3.0
+ */
+
+$GLOBALS['TL_LANG']['tl_iso_config']['klarna_terms_page'][0]        = 'Terms and Conditions';
+$GLOBALS['TL_LANG']['tl_iso_config']['klarna_terms_page'][1]        = 'Pick the page containing your shop\'s terms and conditions.';
+$GLOBALS['TL_LANG']['tl_iso_config']['klarna_checkout_page'][0]     = 'Checkout';
+$GLOBALS['TL_LANG']['tl_iso_config']['klarna_checkout_page'][1]     = 'Pick the page containing the checkout.';
+$GLOBALS['TL_LANG']['tl_iso_config']['klarna_confirmation_page'][0] = 'Confirmation';
+$GLOBALS['TL_LANG']['tl_iso_config']['klarna_confirmation_page'][1] = 'Pick the page containing the checkout confirmation that the customer get redirected to after successful order.';
+$GLOBALS['TL_LANG']['tl_iso_config']['klarna_cancellation_page'][0] = 'Cancellation';
+$GLOBALS['TL_LANG']['tl_iso_config']['klarna_cancellation_page'][1] = 'Pick the page the customer get redirected after cancelling the checkout.';
