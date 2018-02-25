93d0b30c38e50c81a1ae0d70b198d06e94819d84
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2018-02-26 00:16:05 +0100

    Add config files.

---
+++ contao/config/autoload.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2018 Leo Feyer
+ *
+ * @license LGPL-3.0+
+ */
+
+
+/**
+ * Register the templates
+ */
+TemplateLoader::addFiles(array
+(
+	'mod_klarna_checkout'              => 'system/modules/isotope-klarna-checkout/templates',
+	'mod_klarna_checkout_confirmation' => 'system/modules/isotope-klarna-checkout/templates',
+));
