a76af0f2cfc5060b25cb9dbd7c53d2d4509e25e6
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2018-02-26 23:09:22 +0100

    Set cart to prevent errors within the Isotope logic.

--- src/Controller/AddressUpdate.php
+++ src/Controller/AddressUpdate.php
@@ -17,6 +17,7 @@ namespace Richardhj\IsotopeKlarnaCheckoutBundle\Controller;
 use Contao\Model;
 use Contao\ModuleModel;
 use Contao\PageError404;
+use Isotope\Isotope;
 use Isotope\Model\Address;
 use Isotope\Model\ProductCollection\Cart;
 use Richardhj\IsotopeKlarnaCheckoutBundle\Util\GetOrderLinesTrait;
@@ -68,6 +69,9 @@ class AddressUpdate
 
         $this->cart->setShippingAddress($address);
         $this->cart->save();
+
+        // Set cart to prevent errors within the Isotope logic.
+        Isotope::setCart($this->cart);
 
         // Since we updated the shipping address, now we can fetch the current shipping methods.
         $shippingOptions = $this->shippingOptions(deserialize($checkoutModule->iso_shipping_modules, true));
