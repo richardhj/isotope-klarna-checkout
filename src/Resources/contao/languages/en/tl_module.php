3606ad83cb3c3ca189e9240baabccd51ddf1e764
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2018-02-27 18:45:00 +0100

    Add translations.

--- src/Resources/contao/languages/en/tl_module.php
+++ src/Resources/contao/languages/en/tl_module.php
@@ -11,13 +11,27 @@
  * @license   https://github.com/richardhj/isotope-klarna-checkout/blob/master/LICENSE LGPL-3.0
  */
 
-$GLOBALS['TL_LANG']['tl_module']['klarna_show_subtotal_detail'][0]  = 'Show detailed subtotals';
-$GLOBALS['TL_LANG']['tl_module']['klarna_show_subtotal_detail'][1]  = 'If true, the Order Detail subtotals view is expanded.';
-$GLOBALS['TL_LANG']['tl_module']['klarna_terms_page'][0]            = 'Terms and Conditions';
-$GLOBALS['TL_LANG']['tl_module']['klarna_terms_page'][1]            = 'Pick the page containing your shop\'s terms and conditions.';
-$GLOBALS['TL_LANG']['tl_module']['klarna_checkout_page'][0]         = 'Checkout';
-$GLOBALS['TL_LANG']['tl_module']['klarna_checkout_page'][1]         = 'Pick the page containing the checkout.';
-$GLOBALS['TL_LANG']['tl_module']['klarna_confirmation_page'][0]     = 'Confirmation';
-$GLOBALS['TL_LANG']['tl_module']['klarna_confirmation_page'][1]     = 'Pick the page containing the checkout confirmation that the customer get redirected to after successful order.';
-$GLOBALS['TL_LANG']['tl_module']['klarna_cancellation_page'][0]     = 'Cancellation';
-$GLOBALS['TL_LANG']['tl_module']['klarna_cancellation_page'][1]     = 'Pick the page the customer get redirected after cancelling the checkout.';
+$GLOBALS['TL_LANG']['tl_module']['customization_legend'] = 'Customization';
+
+$GLOBALS['TL_LANG']['tl_module']['klarna_show_subtotal_detail'][0]     = 'Show detailed subtotals';
+$GLOBALS['TL_LANG']['tl_module']['klarna_show_subtotal_detail'][1]     = 'If true, the Order Detail subtotals view is expanded.';
+$GLOBALS['TL_LANG']['tl_module']['klarna_terms_page'][0]               = 'Terms and Conditions';
+$GLOBALS['TL_LANG']['tl_module']['klarna_terms_page'][1]               = 'Pick the page containing your shop\'s terms and conditions.';
+$GLOBALS['TL_LANG']['tl_module']['klarna_checkout_page'][0]            = 'Checkout';
+$GLOBALS['TL_LANG']['tl_module']['klarna_checkout_page'][1]            = 'Pick the page containing the checkout.';
+$GLOBALS['TL_LANG']['tl_module']['klarna_confirmation_page'][0]        = 'Confirmation';
+$GLOBALS['TL_LANG']['tl_module']['klarna_confirmation_page'][1]        = 'Pick the page containing the checkout confirmation that the customer get redirected to after successful order.';
+$GLOBALS['TL_LANG']['tl_module']['klarna_cancellation_page'][0]        = 'Cancellation';
+$GLOBALS['TL_LANG']['tl_module']['klarna_cancellation_page'][1]        = 'Pick the page the customer get redirected after cancelling the checkout.';
+$GLOBALS['TL_LANG']['tl_module']['klarna_color_button'][0]             = 'Button color';
+$GLOBALS['TL_LANG']['tl_module']['klarna_color_button'][1]             = 'Define the background color of buttons within the checkout iFrame.';
+$GLOBALS['TL_LANG']['tl_module']['klarna_color_button_text'][0]        = 'Button text color';
+$GLOBALS['TL_LANG']['tl_module']['klarna_color_button_text'][1]        = 'Define the text color of buttons within the checkout iFrame.';
+$GLOBALS['TL_LANG']['tl_module']['klarna_color_checkbox'][0]           = 'Checkbox color';
+$GLOBALS['TL_LANG']['tl_module']['klarna_color_checkbox'][1]           = 'Define the background color of checkboxes within the checkout iFrame.';
+$GLOBALS['TL_LANG']['tl_module']['klarna_color_checkbox_checkmark'][0] = 'Checkbox checkmark color';
+$GLOBALS['TL_LANG']['tl_module']['klarna_color_checkbox_checkmark'][1] = 'Define the checkmark color within the checkout iFrame.';
+$GLOBALS['TL_LANG']['tl_module']['klarna_color_header'][0]             = 'Header color';
+$GLOBALS['TL_LANG']['tl_module']['klarna_color_header'][1]             = 'Define the color of the header within the checkout iFrame.';
+$GLOBALS['TL_LANG']['tl_module']['klarna_color_link'][0]               = 'Link color';
+$GLOBALS['TL_LANG']['tl_module']['klarna_color_link'][1]               = 'Define the color of hyperlinks within the checkout iFrame.';
