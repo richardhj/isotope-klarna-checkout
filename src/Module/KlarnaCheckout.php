7913ec0aeaec2e612469cec715dda6814f15d0a7
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2018-04-09 20:07:18 +0200

    Fix argument type.

--- src/Module/KlarnaCheckout.php
+++ src/Module/KlarnaCheckout.php
@@ -410,7 +410,7 @@ class KlarnaCheckout extends Module
                 $checkoutForm = $isotopeOrder->getPaymentMethod()->checkoutForm($isotopeOrder, $this);
                 if (false === $checkoutForm) {
                     throw new RedirectResponseException(
-                        NativeCheckout::generateUrlForStep(NativeCheckout::STEP_COMPLETE, $isotopeOrder->getUniqueId())
+                        NativeCheckout::generateUrlForStep(NativeCheckout::STEP_COMPLETE, $isotopeOrder)
                     );
                 }
 
