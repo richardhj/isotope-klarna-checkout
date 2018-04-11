a68aa1bd88606305dff4cc4bdf353467f4a7f388
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2018-04-11 15:35:31 +0200

    Fix language key.

--- src/Resources/contao/languages/en/tl_iso_shipping.php
+++ src/Resources/contao/languages/en/tl_iso_shipping.php
@@ -14,13 +14,13 @@
 
 use Richardhj\IsotopeKlarnaCheckoutBundle\Util\ShippingOption;
 
-$GLOBALS['TL_LANG']['tl_iso_config']['klarna_legend'] = 'Klarna';
+$GLOBALS['TL_LANG']['tl_iso_shipping']['klarna_legend'] = 'Klarna';
 
-$GLOBALS['TL_LANG']['tl_iso_config']['klarna_shipping_method'][0] = 'Shipping type';
-$GLOBALS['TL_LANG']['tl_iso_config']['klarna_shipping_method'][1] = 'Classify this shipping method by Klarna provided categories (if any matches).';
+$GLOBALS['TL_LANG']['tl_iso_shipping']['klarna_shipping_method'][0] = 'Shipping type';
+$GLOBALS['TL_LANG']['tl_iso_shipping']['klarna_shipping_method'][1] = 'Classify this shipping method by Klarna provided categories (if any matches).';
 
-$GLOBALS['TL_LANG']['tl_iso_config']['klarna_shipping_methods'][ShippingOption::METHOD_PICK_UP_STORE] = 'Pick up store';
-$GLOBALS['TL_LANG']['tl_iso_config']['klarna_shipping_methods'][ShippingOption::METHOD_HOME]          = 'Home delivery';
-$GLOBALS['TL_LANG']['tl_iso_config']['klarna_shipping_methods'][ShippingOption::METHOD_BOX_REG]       = 'Registered box';
-$GLOBALS['TL_LANG']['tl_iso_config']['klarna_shipping_methods'][ShippingOption::METHOD_BOX_UNREG]     = 'Unregistered box';
-$GLOBALS['TL_LANG']['tl_iso_config']['klarna_shipping_methods'][ShippingOption::METHOD_PICK_UP_POINT] = 'Pick up point';
+$GLOBALS['TL_LANG']['tl_iso_shipping']['klarna_shipping_methods'][ShippingOption::METHOD_PICK_UP_STORE] = 'Pick up store';
+$GLOBALS['TL_LANG']['tl_iso_shipping']['klarna_shipping_methods'][ShippingOption::METHOD_HOME]          = 'Home delivery';
+$GLOBALS['TL_LANG']['tl_iso_shipping']['klarna_shipping_methods'][ShippingOption::METHOD_BOX_REG]       = 'Registered box';
+$GLOBALS['TL_LANG']['tl_iso_shipping']['klarna_shipping_methods'][ShippingOption::METHOD_BOX_UNREG]     = 'Unregistered box';
+$GLOBALS['TL_LANG']['tl_iso_shipping']['klarna_shipping_methods'][ShippingOption::METHOD_PICK_UP_POINT] = 'Pick up point';
